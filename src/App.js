dca47490b34385d40dbfe75411ba02c2e85cb813
VictoriaStarynchuk <91877111+VictoriaStarynchuk@users.noreply.github.com>
2022-12-09 00:09:02 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/map" element={<MapPage />} />
         </Routes>
