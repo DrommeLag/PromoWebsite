b0212a0fb067907a049344d4cfcd56b1975df95a
Roksolana Mazurak <roksamazurak@gmail.com>
2022-12-08 23:38:27 +0200

    Create CharacterModal.js

---
+++ src/components/CharacterModal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+
+function CharacterModal() {
+  return (
+    <div className=" d-flex justify-content-between">
+      <div className="bg-light p-3 rounded">
+        Досліджуй Україну за допомогою інтерактивної карти! Для повного
+        функціоналу, скачуй додаток за QR кодом нижче!
+      </div>
+      <Image src="images/character.svg" alt="Character" />
+    </div>
+  );
+}
+
+export default CharacterModal;
