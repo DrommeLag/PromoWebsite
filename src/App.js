6bd48215ac3c5396e5471a9bb17a774f1a1a68cf
VictoriaStarynchuk <91877111+VictoriaStarynchuk@users.noreply.github.com>
2022-12-08 22:55:45 +0200

    ui for about-us page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import MapPage from "./pages/MapPage";
-import ViewPointSection from "./components/ViewPointSection";
 
 function App() {
   return (
     <>
       <Router>
-        <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/map" element={<MapPage />} />
-          <Route path="/lviv" element={<ViewPointSection />} />
         </Routes>
       </Router>
     </>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import AboutUs from "../components/about_page/AboutUs";
+import NavBar from "../components/nav/NavBar";
+import NavFooter from "../components/nav/NavFooter";
 
 function About() {
-  return <></>;
+  return <>
+      <NavBar />
+      <AboutUs />
+      <NavFooter />
+  </>;
 }
 
 export default About;
