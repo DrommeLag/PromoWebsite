0a0288a6fd88d553ff5bf7fbcaaa624cd73c5fd5
VictoriaStarynchuk <91877111+VictoriaStarynchuk@users.noreply.github.com>
2022-12-09 02:03:13 +0200

    updated styles

--- src/components/about_page/AboutUs.js
+++ src/components/about_page/AboutUs.js
@@ -1,29 +1,30 @@
 import React from "react";
 import "./AboutUs.css";
+import "../Styles.css";
 
 function Questions(){
     return (
         <>
         <div className="qs">
             <div className="text-q">
-                <h1>Що ти отримуєш ?</h1>
+                <h1 className="h-text">Що ти отримуєш ?</h1>
                 <span id="line"><img alt="line" src='images/main_page/line_score.svg'/></span>
-                <p id="q-p">Вся країна у твоєму телефоні. Єдиний в Україні додаток, з подібним функціоналом, зручний інтерфейс та інтерактивність. </p>
+                <p className="p_landing_about" id="q-p">Вся країна у твоєму телефоні. Єдиний в Україні додаток, з подібним функціоналом, зручний інтерфейс та інтерактивність. </p>
             </div>
             <img  id="bubbles1" src="images/about_page/bubbles1.svg" alt="icon"/>
         </div>
         <div className="qs">
             <img  id="bubbles2" src="images/about_page/bubbles2.svg" alt="icon"/>
             <div className="text-q">
-                <h1>Чому ми ?</h1>
+                <h1 className="h-text">Чому ми ?</h1>
                 <span id="line"><img alt="line" src='images/main_page/line_score.svg'/></span>
-                <p id="q-p">Ми команда студентів програми Internet of Things із Lviv Polytechnic National University.
+                <p className="p_landing_about" id="q-p">Ми команда студентів програми Internet of Things із Lviv Polytechnic National University.
                     Наша мета популяризувати та розвивати українське.
                     Ми вирішили створити нашу державу у телефоні, щоб кожен зміг побачити всю красу України.  Також завдяки UApp ми допомагаємо людям із східних регіонів позбутись мовного бар’єру.</p>
             </div>
         </div>
         <div id="team-text">
-            <h1>Команда розробників</h1>
+            <h1 className="h-text">Команда розробників</h1>
             <span id="line"><img alt="line" src='images/main_page/line_score.svg'/></span>
         </div>
         <div className="team">
@@ -64,16 +65,16 @@ function Questions(){
             </div>
         </div>
         <div className="cooperation">
-            <h1>Хочеш співпрацювати?</h1>
-            <p id="coop-p">Ми відкриті до рекламних пропозицій від українських брендів, для розміщення продуктів на нашому маркетплейсі. </p>
+            <h1 className="h-text">Хочеш співпрацювати?</h1>
+            <p className="p_landing_about" id="coop-p">Ми відкриті до рекламних пропозицій від українських брендів, для розміщення продуктів на нашому маркетплейсі. </p>
             <div id="coop-btn">
-                <button className="button">
+                <button className="button_landing_about">
                     <a href="drommelag@gmail.com">Підтримати команду</a>
                 </button>
             </div>
         </div>
         <div className="ask_form">
-            <h1>Маєш якісь запитання?</h1>
+            <h1 className="h-text">Маєш якісь запитання?</h1>
             <span id="line"><img alt="line" src='images/main_page/line_score.svg'/></span>
             <div className="input_form">
                 <div className="boxes">
@@ -86,7 +87,7 @@ function Questions(){
                     <input type="your-text" placeholder="Твоє повідомлення" id="input-ypur-text" required />
                 </div>
                 <div id="coop-btn">
-                <button className="button">
+                <button className="button_landing_about">
                     <a href="drommelag@gmail.com">Напиши нам</a>
                 </button>
             </div>
@@ -97,7 +98,7 @@ function Questions(){
             <div className="blue-boxes">
                 <div className="box">
                     <h5 id="box-question">Додаток безкоштовний ?</h5>
-                    <p id="box-answer">Так, кожен може користуватись додатком просто скачавши його.</p>
+                    <p className="p_landing_about" id="box-answer">Так, кожен може користуватись додатком просто скачавши його.</p>
                 </div>
                 <div className="box">
                     <h5 id="box-question">Чи доступна версія для IOS ?</h5>
